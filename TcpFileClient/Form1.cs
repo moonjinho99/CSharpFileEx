fb3dc7217334bb05ade35110e8b4d5a6eb65ebb9
mjh <mjh@DESKTOP-KKPNTN4>
2024-04-15 15:39:51 +0900

    초기 커밋.

---
+++ TcpFileClient/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TcpFileClient
+{
+    public partial class Form1 : Form
+    {
+        string m_spliter = "'\\'";
+        string m_fName = string.Empty;
+        string[] m_split = null;
+        private const int ChunkSize = 1024; // 작은 데이터 조각의 크기
+
+        enum DataPacketType { TEXT = 1, IMAGE };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Console.WriteLine("===클라이언트===");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char[] delimeter = m_spliter.ToCharArray();
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+            openFileDialog.ShowDialog();
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            textBox1.Text = openFileDialog.FileName;
+            pictureBox1.ImageLocation = openFileDialog.FileName;
+
+            m_split = textBox1.Text.Split(delimeter);
+            int limit = m_split.Length;
+
+            m_fName = m_split[limit - 1].ToString();
+
+            if (textBox1.Text != null)
+                button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                byte[] fileName = Encoding.UTF8.GetBytes(m_fName);
+                byte[] fileData = File.ReadAllBytes(textBox1.Text);
+                byte[] fileNameLen = BitConverter.GetBytes(fileName.Length);
+                byte[] fileType = BitConverter.GetBytes((int)DataPacketType.IMAGE);
+
+                // 서버의 IP 주소와 포트 번호
+                IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse("192.168.56.1"), 9050);
+
+                // 파일 전송 시작 메시지 전송
+                clientSocket.SendTo(BitConverter.GetBytes((int)DataPacketType.IMAGE), serverEP);
+                clientSocket.SendTo(fileNameLen, serverEP);
+                clientSocket.SendTo(fileName, serverEP);
+
+                // 데이터를 조각으로 나누어 전송
+                int totalChunks = (int)Math.Ceiling((double)fileData.Length / ChunkSize);
+                for (int i = 0; i < totalChunks; i++)
+                {
+                    int offset = i * ChunkSize;
+                    int size = Math.Min(ChunkSize, fileData.Length - offset);
+                    byte[] chunkData = new byte[size];
+                    Array.Copy(fileData, offset, chunkData, 0, size);
+                    clientSocket.SendTo(chunkData, serverEP);
+                }
+
+                // 파일 전송 완료 메시지 전송
+                clientSocket.SendTo(BitConverter.GetBytes((int)DataPacketType.IMAGE), serverEP);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+               
+                byte[] textData = Encoding.UTF8.GetBytes(textBox2.Text);
+                byte[] fileType = BitConverter.GetBytes((int)DataPacketType.TEXT);
+
+                // 서버의 IP 주소와 포트 번호
+                IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse("192.168.56.1"), 9050);
+
+                // 텍스트 전송 시작 메시지 전송
+                clientSocket.SendTo(BitConverter.GetBytes((int)DataPacketType.TEXT), serverEP);
+
+                // 텍스트 데이터 전송
+                clientSocket.SendTo(textData, serverEP);
+
+                // 텍스트 전송 완료 메시지 전송
+                clientSocket.SendTo(BitConverter.GetBytes((int)DataPacketType.TEXT), serverEP);
+            }
+        }
+    }
+}
---
+++ TcpFileExServer/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Diagnostics;
+
+namespace TcpFileExServer
+{
+    public partial class Form1 : Form
+    {
+
+        bool initialFlag = true;
+        string receivedPath = string.Empty;
+        enum DataPacketType { TEXT = 1, IMAGE };
+        int dataType = 0;
+        string textData = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Console.WriteLine("===서버===");
+            Thread t_handler = new Thread(StartListening);
+            t_handler.IsBackground = true;
+            t_handler.Start();
+        }
+
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+        private void StartListening()
+        {
+            // 서버에서 TCP 대신 UDP를 사용합니다.
+            IPEndPoint localEP = new IPEndPoint(IPAddress.Any, 9050);
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            StateObject state = new StateObject();
+            state.workSocket = listener;
+
+            try
+            {
+                listener.Bind(localEP);
+
+                while (true)
+                {
+                    allDone.Reset();
+                    // 원격 호스트의 IP 주소와 포트 번호를 저장할 EndPoint 객체를 생성합니다.
+                    EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                    // UDP에서는 BeginReceiveFrom 메서드를 사용하여 데이터를 수신합니다.
+                    listener.BeginReceiveFrom(state.buffer, 0, StateObject.BufferSize, 0, ref remoteEP, new AsyncCallback(ReadCallback), state);
+                    allDone.WaitOne();
+                }
+            }
+            catch (SocketException se)
+            {
+                Trace.WriteLine(string.Format("SocketException : {0}", se.Message));
+                Console.WriteLine(string.Format("SocketException : {0}", se.Message));
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(string.Format("Exception : {0}", ex.Message));
+                Console.WriteLine(string.Format("Exception : {0}", ex.Message));
+            }
+        }
+
+        private void ReadCallback(IAsyncResult ar)
+        {
+            int fileNameLen = 0;
+            StateObject state = ar.AsyncState as StateObject;
+            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+            try
+            {
+                int bytesRead = state.workSocket.EndReceiveFrom(ar, ref remoteEP);
+
+                if (bytesRead > 0)
+                {
+                    if (initialFlag)
+                    {
+                        byte[] buffer = state.buffer;
+
+                        dataType = BitConverter.ToInt32(buffer, 0);
+
+                        if (dataType == (int)DataPacketType.IMAGE)
+                        {
+                            fileNameLen = BitConverter.ToInt32(buffer, 4);
+                            string fileName = Encoding.UTF8.GetString(buffer, 8, fileNameLen);
+
+                            string pathUser = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                            string pathDownload = Path.Combine(pathUser, "Downloads");
+                            
+                            receivedPath = Path.Combine(pathDownload, fileName);
+
+                            if (File.Exists(receivedPath))
+                                File.Delete(receivedPath);
+                        }
+                        else if (dataType == (int)DataPacketType.TEXT)
+                        {
+                            Console.WriteLine("바이트수 : " + bytesRead);
+                            textData = Encoding.UTF8.GetString(buffer, 4, bytesRead - 4);
+                            state.workSocket.BeginReceiveFrom(state.buffer, 0, StateObject.BufferSize, 0, ref remoteEP, new AsyncCallback(ReadCallback), state);
+                        }
+                    }
+
+                    if (dataType == (int)DataPacketType.IMAGE)
+                    {
+                        BinaryWriter bw = new BinaryWriter(File.Open(receivedPath, FileMode.Append));
+                        if (initialFlag)
+                            bw.Write(state.buffer, 8 + fileNameLen, bytesRead - (8 + fileNameLen));
+                        else
+                            bw.Write(state.buffer, 0, bytesRead);
+
+                        initialFlag = false;
+                        bw.Close();
+                        state.workSocket.BeginReceiveFrom(state.buffer, 0, StateObject.BufferSize, 0, ref remoteEP, new AsyncCallback(ReadCallback), state);
+                    }
+                }
+                else
+                {
+                    if (dataType == (int)DataPacketType.IMAGE)
+                    {
+                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                        pictureBox1.ImageLocation = receivedPath;
+                        Invoke((MethodInvoker)delegate
+                        {
+                            label1.Text = "Data has been received";
+                        });
+                    }
+                    else if (dataType == (int)DataPacketType.TEXT)
+                        Invoke((MethodInvoker)delegate
+                        {
+                            Console.WriteLine("출력");
+                            textBox1.Text = textData;
+                        });
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ReadCallback 오류: " + e.ToString());
+            }
+            finally
+            {
+                allDone.Set();
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
